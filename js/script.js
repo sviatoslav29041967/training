6c01ccc5c5c5b052f3ab868d30df8b5d6c12667b
Sviatoslav <sviatoslav29041967@gmail.com>
2022-02-19 16:11:21 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+" use strict ";
+
+// console.log('arr' + ' - obj');
+// console.log(4 + ' - obj');
+// // console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+    // incr++;
+    // decr--;
+    // console.log(++incr);
+    // console.log(--decr);
+    // console.log(incr++);
+    // console.log(decr--);
+    // console.log(incr++);
+    // console.log(decr--);
+    // console.log(incr++);
+    // console.log(decr--);
+
+    // console.log(12 % 2);
+
+    // console.log(2 * 4 == "8");
+    // console.log(2 * 4 === 8);
+     
+  const isChecked = false,
+        isClose = false;
+        console.log(isChecked || !isClose); 
